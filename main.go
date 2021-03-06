d0147977e37317f5588b682d329797afe1a0d3e7
Reaper <ahoy@barelyhuman.dev>
2021-03-06 17:31:56 +0530

    feat(log): add commit scope support (#7)

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	var repoPath = flag.String("p", ".", "path to the repository, points to the current working directory by default")
 	var startCommit = flag.String("s", "", "commit hash string / revision (ex. HEAD, HEAD^, HEAD~2) \n to start collecting commit messages from")
 	var endCommit = flag.String("e", "", "commit hash string / revision (ex. HEAD, HEAD^, HEAD~2) \n to stop collecting commit message at")
-	var inclusionFlags = flag.String("i", "ci,refactor,docs,fix,feat,test,chore,other", "commit types to be includes")
+	var inclusionFlags = flag.String("i", clog.SupportedKeys, "commit types to be includes")
 	var skipClassification = flag.Bool("skip", false, "if true will skip trying to classify and just give a list of changes")
 
 	flag.Parse()
