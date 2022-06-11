e3445f430b8deaf474339b78100d1f69cfb1c1ac
Reaper <ahoy@barelyhuman.dev>
2022-06-11 17:49:49 +0530

    feat: generator subcommand implementation
    implement the base set of functionality for the
    generator command
    TODO: should improve the naming of the internal structs

--- commands/commitlog.go
+++ commands/commitlog.go
@@ -1,7 +1,70 @@
 package commands
 
-import "github.com/urfave/cli/v2"
+import (
+	"github.com/barelyhuman/commitlog/pkg"
+	"github.com/urfave/cli/v2"
+)
 
 func Commitlog(c *cli.Context) (err error) {
-	return
+	path := c.String("path")
+	addPromo := c.Bool("promo")
+	out := c.String("out")
+	stdio := c.Bool("stdio")
+	startRef := c.String("start")
+	endRef := c.String("end")
+	categories := c.String("categories")
+
+	gOptions := []pkg.GeneratorConfigMod{}
+
+	if addPromo {
+		gOptions = append(gOptions,
+			pkg.WithPromo(),
+		)
+	}
+
+	// either write to a file or to the stdio with true by default
+	if len(out) > 0 {
+		gOptions = append(gOptions,
+			pkg.WithOutputToFile(out),
+		)
+	} else if stdio {
+		gOptions = append(gOptions,
+			pkg.WithOutputToStdio(),
+		)
+	}
+
+	if len(startRef) > 0 {
+		gOptions = append(gOptions,
+			pkg.WithStartReference(startRef),
+		)
+	}
+
+	if len(endRef) > 0 {
+		gOptions = append(gOptions,
+			pkg.WithEndReference(endRef),
+		)
+	}
+
+	if len(categories) > 0 {
+		gOptions = append(gOptions,
+			pkg.WithCategories(categories),
+		)
+	}
+
+	generator := pkg.CreateGenerator(path,
+		gOptions...)
+
+	err = generator.ReadCommmits()
+
+	if err != nil {
+		return err
+	}
+
+	err = generator.Classify()
+
+	if err != nil {
+		return err
+	}
+
+	return generator.Generate()
 }
---
+++ pkg/generator.go
@@ -0,0 +1,304 @@
+package pkg
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/barelyhuman/commitlog/lib"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+)
+
+type commitClassification struct {
+	key     string
+	commits []string
+}
+
+type Generator struct {
+	repo                *git.Repository
+	workingDirectorPath string
+	startRef            string
+	endRef              string
+	addPromo            bool
+	categories          []string
+	output              struct {
+		stdio    bool
+		file     bool
+		filePath string
+	}
+	classifiedCommits []commitClassification
+	inMarkdown        string
+	commitToProcess   []*object.Commit
+}
+
+type GeneratorConfigMod func(*Generator)
+
+func (g *Generator) openRepo() {
+	if g.repo != nil {
+		return
+	}
+
+	r, err := git.PlainOpen(g.workingDirectorPath)
+	if err != nil {
+		log.Fatal("Error opening Repository: ", err)
+	}
+	g.repo = r
+}
+
+func (g *Generator) readCommitsInTags() (err error) {
+	// make sure the repo is open
+	g.openRepo()
+
+	var commits []*object.Commit
+
+	var latestTagCommit *object.Commit
+	var previousTagCommit *object.Commit
+
+	commitsIter, err := g.repo.Log(&git.LogOptions{})
+	if err != nil {
+		err = fmt.Errorf("[commitlog] Failed to get commits from given hash %v", err)
+		return
+	}
+	defer commitsIter.Close()
+
+	// loop through the commits to get the
+	// most recent tagged commit and the 2nd most recent
+	// tagged commit
+	for {
+		c, err := commitsIter.Next()
+
+		if err == io.EOF {
+			break
+		}
+
+		isTag := lib.IsHashATag(g.repo, c.Hash)
+		if isTag {
+			if latestTagCommit == nil {
+				latestTagCommit = c
+			} else if previousTagCommit == nil {
+				previousTagCommit = c
+				break
+			}
+		}
+	}
+
+	commitsIter, err = g.repo.Log(&git.LogOptions{From: latestTagCommit.Hash})
+
+	if err != nil {
+		err = fmt.Errorf("[commitlog] Failed to get commits from given hash %v", err)
+		return
+	}
+	defer commitsIter.Close()
+
+	for {
+		c, err := commitsIter.Next()
+
+		if err == io.EOF {
+			break
+		}
+
+		// do not include the last matching commit
+		if previousTagCommit != nil && c.Hash == previousTagCommit.Hash {
+			break
+		}
+
+		commits = append(commits, c)
+	}
+
+	// this will either have commits between 2 tags or all commits if no tags exist
+	g.commitToProcess = commits
+
+	return
+
+}
+
+func (g *Generator) readCommitsInRange() (err error) {
+	// make sure the repo is open
+	g.openRepo()
+
+	var commits []*object.Commit
+	var commitsIter object.CommitIter
+
+	startHash := lib.GetCommitFromString(g.repo, g.startRef)
+	endHash := lib.GetCommitFromString(g.repo, g.endRef)
+
+	if startHash != nil {
+		commitsIter, err = g.repo.Log(&git.LogOptions{From: startHash.Hash})
+	} else {
+		commitsIter, err = g.repo.Log(&git.LogOptions{})
+	}
+
+	if err != nil {
+		err = fmt.Errorf("[commitlog] Failed to get commits from given hash %v", err)
+		return
+	}
+
+	for {
+		c, err := commitsIter.Next()
+
+		if err == io.EOF {
+			break
+		}
+
+		if endHash != nil && c.Hash == endHash.Hash {
+			break
+		}
+
+		commits = append(commits, c)
+	}
+
+	g.commitToProcess = commits
+
+	return
+
+}
+
+// ReadCommmits will try to collect commits in the given range
+// or default to current tag to the previous tag or current commit to
+// recent tag, in the same order of priority
+func (g *Generator) ReadCommmits() (err error) {
+	// make sure the repo is open
+	g.openRepo()
+
+	if len(g.startRef) > 0 || len(g.endRef) > 0 {
+		g.readCommitsInRange()
+	} else {
+		g.readCommitsInTags()
+	}
+
+	return
+}
+
+func (g *Generator) Classify() (err error) {
+	// write the classification using the existing commits
+	if len(g.categories) == 0 {
+		allCommits := []string{}
+
+		for _, commit := range g.commitToProcess {
+			allCommits = append(allCommits, lib.CommitToLog(commit))
+		}
+
+		g.classifiedCommits = []commitClassification{
+			{
+				key:     "All Changes",
+				commits: allCommits,
+			},
+		}
+
+		return
+	}
+
+	for _, catg := range g.categories {
+		rgx, err := regexp.Compile(catg)
+		if err != nil {
+			break
+		}
+
+		catgCommits := []string{}
+
+		for _, commit := range g.commitToProcess {
+			if !rgx.Match([]byte(commit.Message)) {
+				continue
+			}
+
+			catgCommits = append(catgCommits, lib.CommitToLog(commit))
+		}
+
+		g.classifiedCommits = append(g.classifiedCommits, commitClassification{
+			key:     catg,
+			commits: catgCommits,
+		})
+	}
+
+	return
+
+}
+
+func (g *Generator) Generate() (err error) {
+
+	var inMardown strings.Builder
+
+	for _, class := range g.classifiedCommits {
+
+		// title
+		inMardown.Write([]byte("##"))
+		inMardown.Write([]byte(" "))
+		inMardown.Write([]byte(class.key))
+
+		inMardown.Write([]byte("\n"))
+
+		// each commit with 2 returns to separate the long description one's
+		for _, commitMsg := range class.commits {
+			inMardown.Write([]byte(commitMsg))
+			inMardown.Write([]byte("\n\n"))
+		}
+
+	}
+
+	if g.output.stdio {
+		fmt.Println(inMardown.String())
+	}
+
+	if g.output.file {
+		err = os.WriteFile(g.output.filePath, []byte(inMardown.String()), os.ModePerm)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func CreateGenerator(path string, mods ...GeneratorConfigMod) *Generator {
+	generator := &Generator{
+		workingDirectorPath: path,
+	}
+
+	for _, mod := range mods {
+		mod(generator)
+	}
+
+	return generator
+}
+
+func WithPromo() GeneratorConfigMod {
+	return func(g *Generator) {
+		g.addPromo = true
+	}
+}
+
+func WithOutputToFile(filePath string) GeneratorConfigMod {
+	return func(g *Generator) {
+		g.output.file = true
+		g.output.filePath = filePath
+	}
+}
+
+func WithOutputToStdio() GeneratorConfigMod {
+	return func(g *Generator) {
+		g.output.stdio = true
+	}
+}
+
+func WithStartReference(startRef string) GeneratorConfigMod {
+	return func(g *Generator) {
+		g.startRef = startRef
+	}
+}
+
+func WithEndReference(endRef string) GeneratorConfigMod {
+	return func(g *Generator) {
+		g.endRef = endRef
+	}
+}
+
+func WithCategories(categories string) GeneratorConfigMod {
+	return func(g *Generator) {
+		parts := strings.Split(categories, ",")
+		g.categories = parts
+	}
+}
