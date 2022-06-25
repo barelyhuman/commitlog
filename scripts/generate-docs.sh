0a946f6d1ca4cd3d9b70d944feec6a7de294e9be
Reaper <ahoy@barelyhuman.dev>
2022-06-25 20:13:13 +0530

    chore: use baseurl flag on mudkip

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -6,9 +6,6 @@ curl -o mudkip.tgz -L https://github.com/barelyhuman/mudkip/releases/latest/down
 tar -xvzf mudkip.tgz
 install linux-amd64/mudkip /usr/local/bin
 
-# hack for github pages till mudkip doesn't add baseURL functionality
-sed -i s#/#/commitlog/#g ./docs/_sidebar.md
-
-mudkip --stylesheet="./docs/styles.css"
+mudkip --stylesheet="./docs/styles.css" --baseurl='/commitlog/'
 
 
