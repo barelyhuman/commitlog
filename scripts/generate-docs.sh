d669db159928420bea185226a5332985cdc124e7
Reaper <ahoy@barelyhuman.dev>
2022-06-26 18:15:26 +0530

    remove generation to local

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -4,8 +4,6 @@ curl -o mudkip.tgz -L https://github.com/barelyhuman/mudkip/releases/latest/down
 tar -xvzf mudkip.tgz
 install linux-amd64/mudkip /usr/local/bin
 
-./scripts/get-release-urls.sh
-
 mudkip --baseurl='/commitlog/' --stylesheet="./docs/styles.css"
 
 
